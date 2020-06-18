18a1913e0c7d6884741a3874a4a1c84eab81a13a
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-06-18 14:09:45 -0600

    first changes

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -3,8 +3,58 @@ import 'package:flutter/material.dart';
 class DescriptionPlace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final star = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final titleStars = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Text(
+            'Duwili Ella',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: [
+            star,
+            star,
+            star,
+            star,
+            star,
+          ],
+        ),
+      ],
+    );
+
+    final description = Container(
+      height: 50.0,
+      margin: EdgeInsets.all(20.0),
+      child: SingleChildScrollView(
+        child: Text(
+          'Incididunt fugiat irure voluptate officia dolor. Sit pariatur consequat adipisicing officia ex qui Lorem. Proident nisi labore nulla mollit. Fugiat esse et veniam cillum aliqua cillum ea culpa consequat culpa nostrud qui exercitation est. Deserunt adipisicing labore velit id est. Ullamco incididunt labore do nostrud deserunt ut irure elit. Labore dolore consectetur excepteur consectetur adipisicing excepteur ullamco fugiat consequat consequat aute.',
+        ),
+      ),
+    );
+
+    return Column(
+      children: [titleStars, description],
     );
   }
 }
